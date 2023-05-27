0942fc2f728e23281afc59787c6692e06ece6a4c
AndreiDonEsq <andrei1danielescu@gmail.com>
2023-05-27 13:58:12 +0300

    Added button, styles, basic function

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-async function fetchData() {
+const cashbackBtn = document.getElementById("cashbackToggleButton");
+document.addEventListener('DOMContentLoaded', function () {
+    cashbackBtn.addEventListener("click", onCashbackToggle);
+});
 
+
+async function onCashbackToggle() {
+    cashbackBtn.textContent = "Cashback ON";
 }
-fetchData();
