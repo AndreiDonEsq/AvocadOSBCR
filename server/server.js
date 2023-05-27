4610f7fcfadde3b1a7bf725aaeb7fe2bdf34f6f1
teodor <tlupoiu@invisalertsolutions.com>
2023-05-27 21:09:58 +0300

    server work endpoint product

--- server/server.js
+++ server/server.js
@@ -3,16 +3,24 @@ const http = require("http");
 const sqlite3 = require("sqlite3").verbose();
 
 const server = http.createServer((req, res) => {
+    
     res.setHeader(
         "Access-Control-Allow-Origin",
         "chrome-extension://dhgbflmciegpmknahfplcnofcgbgfjge"
     );
+    res.setHeader(
+        'chrome-extension',
+        '//dhgbflmciegpmknahfplcnofcgbgfjge'
+    );
+   
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     if (req.url === "/api/partners" && req.method === "GET") {
         // Handle GET request for /api/partners
         const db = new sqlite3.Database("partners.db");
 
-        db.all("SELECT id, name FROM Partners", (err, rows) => {
+        db.all("SELECT id, name, procent FROM Partners", (err, rows) => {
             if (err) {
                 console.error(err);
                 res.writeHead(500, { "Content-Type": "application/json" });
@@ -63,7 +71,35 @@ const server = http.createServer((req, res) => {
                 }
             );
         });
-    } else {
+    }
+    else if(req.url === "/api/partner" && req.method === "POST"){
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", () => {
+            const db = new sqlite3.Database("partners.db");
+            const { url } = JSON.parse(body);
+            console.log(url)
+            if (!url) {
+                res.writeHead(400, { "Content-Type": "application/json" });
+                res.end(JSON.stringify({ error: "url is required" }));
+                return;
+            }
+
+            db.all(`Select * from Partners p Where p.name like ?`, [`%${url}%`], (error, rows) => {
+                if (error) {
+                    console.error('Error executing query:', error.message);
+                } else {
+                    res.writeHead(200, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({rows}));
+                }
+              });
+        });
+    }
+    else {
         // Handle other routes
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ error: "Not Found" }));
