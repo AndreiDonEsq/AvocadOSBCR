be130c2bf1424641feacd09eef307428f615480c
AndreiDonEsq <andrei1danielescu@gmail.com>
2023-05-27 14:12:04 +0300

    Temporary cashback background script WIP

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,7 @@
 const cashbackBtn = document.getElementById("cashbackToggleButton");
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     cashbackBtn.addEventListener("click", onCashbackToggle);
 });
-
 
 async function onCashbackToggle() {
     cashbackBtn.textContent = "Cashback ON";
