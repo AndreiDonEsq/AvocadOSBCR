203b7b4f0a72bc46ea00060b0d20d7797e4cbec8
teodor <tlupoiu@invisalertsolutions.com>
2023-05-27 18:04:10 +0300

    danciulescu

--- server/server.js
+++ server/server.js
@@ -2,56 +2,16 @@ const http = require("http");
 
 const sqlite3 = require('sqlite3').verbose();
 
-const host = 'localhost';
-const port = 8000;
-
-// const db = new sqlite3.Database('partners.db');
-
-//  db.serialize(function() {
-//   // Create a table
-
-// //   db.run("CREATE TABLE IF NOT EXISTS Partners (id INTEGER PRIMARY KEY, name TEXT)");
-  
-// //   // Insert data into the table
-// //   db.run("INSERT INTO Partners (name) VALUES ('www.emag.ro')");
-// //   db.run("INSERT INTO Partners (name) VALUES ('www.cel.ro')");
-
-//   db.each("SELECT id, name FROM Partners", function(err, row) {
-//     console.log(row.id + ": " + row.name);
-//  });
-// });
-
-// db.close()
-
-
-// const requestListener = function (req, res) {
-//     res.writeHead(200);
-//     const db = new sqlite3.Database('partners.db');
-
-//     const response = [];
-  
-//     db.each("SELECT id, name FROM Partners", function(err, row) {
-//       if (err) {
-//         console.error(err);
-//         return;
-//       }
-//       response.push({"id" : row.id, "Name" : row.name});
-//     }, function() {
-//       res.end(JSON.stringify(response));
-//       db.close();
-//     });
-//   };
-  
-
-// const server = http.createServer(requestListener);
-// server.listen(port, host, () => {
-//     console.log(`Server is running on http://${host}:${port}`);
-// });
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://dhgbflmciegpmknahfplcnofcgbgfjge');
+  
     if (req.url === '/api/partners' && req.method === 'GET') {
       // Handle GET request for /api/partners
       const db = new sqlite3.Database('partners.db');
+
+      
+    
       
       db.all('SELECT id, name FROM Partners', (err, rows) => {
         if (err) {
@@ -75,7 +35,7 @@ const server = http.createServer((req, res) => {
       
       req.on('end', () => {
         const db = new sqlite3.Database('partners.db');
-        const { name } = JSON.parse(body);
+        const { name , procent } = JSON.parse(body);
         
         if (!name) {
           res.writeHead(400, { 'Content-Type': 'application/json' });
