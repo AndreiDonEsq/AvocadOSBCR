ed06d8ce815af0f2a0ea48f63f2521ff8c3ec51d
MonicaMM24 <monica.mager123@gmail.com>
2023-05-28 03:57:05 +0300

    structura intrebari

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,10 @@ const messages = [
             "Give clear answers, however try to act a little bit like a salesman as well. "+
             "Ask the person if they want to buy something. "+
             "You don't represent a shop. "+
-            "Based on what the person wants to buy, aproximate the price of that item ",
+            "Based on what the person wants to buy, aproximate the price of that item. "+
+            "Ask details about the requestor's monthly income and expenses, but wait for questions regarding credits first. "+
+            "A requestor's credit must be less than 40 percent of their income. "+
+            "Do not offer a credit if the requestor has more expenses than income, but offer financial advice to improve their financial situation",
     },
 ];
 
