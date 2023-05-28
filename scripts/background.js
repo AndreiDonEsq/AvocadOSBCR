c2d0b229f5b9256778b88d4ef80a82e5b9418b25
AndreiDonEsq <andrei1danielescu@gmail.com>
2023-05-28 03:35:22 +0300

    formatting

--- scripts/background.js
+++ scripts/background.js
@@ -3,33 +3,28 @@
 // Function to handle the context menu click event
 function handleContextMenuClick(info, tab) {
     // Perform the desired action when the menu item is clicked
-    
-    console.log('Context menu item clicked!');
-    console.log('Selected text:', info.selectionText);
-    console.log('Page URL:', info.pageUrl);
 
-    
-  }
-  
-  // Create a context menu item
-  chrome.contextMenus.create({
-    id: 'myContextMenu',
-    title: 'Intreaba-l pe Vali',
-    contexts: ['selection', 'page'],
-  });
+    console.log("Context menu item clicked!");
+    console.log("Selected text:", info.selectionText);
+    console.log("Page URL:", info.pageUrl);
+}
 
-  chrome.contextMenus.create({
-    id: 'myContextMenu2',
-    title: 'Intreaba-l pe Vali evirom',
-    contexts: ['selection', 'page'],
-  });
+// Create a context menu item
+chrome.contextMenus.create({
+    id: "myContextMenu",
+    title: "Intreaba-l pe Vali",
+    contexts: ["selection", "page"],
+});
 
+chrome.contextMenus.create({
+    id: "myContextMenu2",
+    title: "Intreaba-l pe Vali evirom",
+    contexts: ["selection", "page"],
+});
 
-  
-  // Add a listener for the context menu item click event
-  chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
+// Add a listener for the context menu item click event
+chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
 
-  chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     // Function to execute in the active tab
-    
-  });
+});
