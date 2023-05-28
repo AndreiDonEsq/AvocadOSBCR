d6b3162f347d6502259873426973e79eff56e71b
AndreiDonEsq <andrei1danielescu@gmail.com>
2023-05-28 07:25:06 +0300

    added big window popup

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,26 @@
-// background.js
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message == "popup_to_background") {
+        console.log(request.message);
+    }
+});
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, 'hi'), { message: "background_to_content" };
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message == "content_to_background") {
+        console.log(request.message);
+    }
+});
 
 // Function to handle the context menu click event
 function handleContextMenuClick(info, tab) {
     // Perform the desired action when the menu item is clicked
-
+    chrome.windows.create({
+        url : "../ui/popup.html",
+        focused : true,
+        type : "popup"});
     console.log("Context menu item clicked!");
     console.log("Selected text:", info.selectionText);
     console.log("Page URL:", info.pageUrl);
