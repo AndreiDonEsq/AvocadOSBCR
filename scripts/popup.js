896dbe5e9c6dcf1eff76372cc02504ecd73dcedc
AndreiDonEsq <andrei1danielescu@gmail.com>
2023-05-28 09:35:05 +0300

    fixed popup, broke loading

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,11 @@
-
-
-  chrome.runtime.sendMessage({text: "hey"}, function(response) {
-    chatGPTRequest("ce parere ai de: " + response +" ?" )
+chrome.runtime.sendMessage({ text: "hey" }, function (response) {
+    createUserMessage("Mi-aș dori o părere legată de " + response + 
+    ".", "Cât costă acest produs?");
     console.log("Response: ", response);
 });
 
-
 const urlReq = "http://localhost:3000";
-
+let idCounter = 0;
 const messages = [
     {
         role: "system",
@@ -39,6 +37,106 @@ async function chatGPTRequest(message) {
             messages.push(data);
             _createValiMessage(data.content);
         });
+}
+
+async function createUserMessage(message, additionalInfo) {
+    /*
+        entireMessageDiv
+            authorDiv
+                authorSpan
+            divider1
+            messageDiv
+            divider2
+            timeDiv
+    */
+    const entireMessageDiv = document.createElement("div");
+    document.getElementById("messages_container").appendChild(entireMessageDiv);
+    entireMessageDiv.classList.add(
+        "chat-log_item",
+        "chat-log_item-own",
+        "z-depth-0"
+    );
+
+    const authorDiv = document.createElement("div");
+    entireMessageDiv.appendChild(authorDiv);
+    authorDiv.classList.add(
+        "row",
+        "justify-content-end",
+        "mx-1",
+        "d-flex",
+        "col-auto",
+        "px-0"
+    );
+
+    const authorSpan = document.createElement("span");
+    authorDiv.appendChild(authorSpan);
+    authorSpan.classList.add("chat-log_author");
+    authorSpan.innerHTML = "You";
+
+    const divider1 = document.createElement("hr");
+    entireMessageDiv.appendChild(divider1);
+    divider1.classList.add("my-1", "py-0", "col-8");
+
+    const messageDiv = document.createElement("div");
+    entireMessageDiv.appendChild(messageDiv);
+    messageDiv.classList.add("chat-log_message");
+    
+    if(message){
+        messageDiv.innerHTML = message;
+        chatGPTRequest(message+additionalInfo);
+    } else {
+        //Read textArea content, set it as message. This will later be sent to ChatGPT.
+        const messageContent = document.getElementById("messageTextArea").value;
+        messageDiv.innerHTML = message;
+        chatGPTRequest(messageContent);
+        //Don't foregt to clear the textarea
+        document.getElementById("messageTextArea").value = "";
+    }
+
+    const divider2 = document.createElement("hr");
+    entireMessageDiv.appendChild(divider2);
+    divider2.classList.add("my-1", "py-0", "col-8");
+
+    //Get current date time and add them
+    let currentDate = new Date(),
+        currentHour = String(currentDate.getHours()),
+        currentMinute = String(currentDate.getMinutes());
+    currentHour = currentHour.length === 1 ? "0" + currentHour : currentHour;
+    currentMinute =
+        currentMinute.length === 1 ? "0" + currentMinute : currentMinute;
+    const timeDiv = document.createElement("div");
+    entireMessageDiv.appendChild(timeDiv);
+    timeDiv.classList.add(
+        "row",
+        "chat-log_time",
+        "m-0",
+        "p-0",
+        "justify-content-end"
+    );
+    timeDiv.innerHTML = `${currentHour}:${currentMinute}`;
+
+    const entireImgMessageDiv = document.createElement("div");
+    document
+        .getElementById("messages_container")
+        .appendChild(entireImgMessageDiv);
+    entireImgMessageDiv.classList.add("chat-log_item_img", "z-depth-0");
+    entireImgMessageDiv.setAttribute("id", "imageLoad" + String(idCounter++));
+
+    const authorImgDiv = document.createElement("div");
+    entireImgMessageDiv.appendChild(authorImgDiv);
+    authorImgDiv.classList.add(
+        "row",
+        "justify-content-end",
+        "mx-1",
+        "d-flex",
+        "col-auto",
+        "px-0"
+    );
+    let loadingImg = document.createElement("img");
+    loadingImg.src = "../images/typing-texting.gif";
+    loadingImg.classList.add("tiny");
+
+    authorImgDiv.appendChild(loadingImg);
 }
 
 async function _createValiMessage(sText) {
@@ -82,6 +180,11 @@ async function _createValiMessage(sText) {
     const divider2 = document.createElement("hr");
     entireMessageDiv.appendChild(divider2);
     divider2.classList.add("my-1", "py-0", "col-8");
+
+    let loadingIndicator = document.getElementById(
+        "imageLoad" + String(idCounter - 1)
+    );
+    loadingIndicator.remove();
 
     //Get current date time and add them
     let currentDate = new Date(),
