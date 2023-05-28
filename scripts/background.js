aafd4b0bedbc489e6ca5b20196029acf5d65a2a1
teodor <tlupoiu@invisalertsolutions.com>
2023-05-28 03:34:03 +0300

    extensie

--- scripts/background.js
+++ scripts/background.js
@@ -3,18 +3,12 @@
 // Function to handle the context menu click event
 function handleContextMenuClick(info, tab) {
     // Perform the desired action when the menu item is clicked
-    console.log(document.getElementsByClassName("Price-int leading-none"))
+    
     console.log('Context menu item clicked!');
     console.log('Selected text:', info.selectionText);
     console.log('Page URL:', info.pageUrl);
 
-    chrome.runtime.onInstalled.addListener(function() {
-        const backgroundPage = chrome.extension.getBackgroundPage();
-        
-        if (backgroundPage) {
-          backgroundPage.myFunction(); // Call the function from external.js
-        }
-      });
+    
   }
   
   // Create a context menu item
@@ -34,3 +28,8 @@ function handleContextMenuClick(info, tab) {
   
   // Add a listener for the context menu item click event
   chrome.contextMenus.onClicked.addListener(handleContextMenuClick);
+
+  chrome.runtime.onInstalled.addListener(function() {
+    // Function to execute in the active tab
+    
+  });
